c8d299cabc2bc7c082a0650b22e375b1bdbc6cd2
Americo Tomas <americotomaswork@gmail.com>
2020-10-30 15:15:43 +0000

    add navigation header and nav links

---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./NavLinks.css";
+
+const NavLinks = (props) => {
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          ALL USERS
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new">ADD PLACE</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
